d0a4b32f8891e03aeca4c5a1684b1a6c134c2585
ipipman <ipipman@163.com>
2021-03-05 23:43:59 +0800

    Add Lessons in Week1

---
+++ Week_1/week1-lessons-project/week1-web-frame/src/main/java/org/ipman/web/mvc/controller/Controller.java
@@ -0,0 +1,23 @@
+package org.ipman.web.mvc.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by ipipman on 2021/3/3.
+ *
+ * @version V1.0
+ *
+ * 页面控制器，负责服务端页面渲染
+ */
+public interface Controller {
+
+    /**
+     * @param request  HTTP 请求
+     * @param response HTTP 相应
+     * @return 视图地址路径
+     * @throws Throwable 异常发生时
+     */
+//    String execute(HttpServletRequest request, HttpServletResponse response) throws Throwable;
+
+}
---
+++ Week_1/week1-lessons-project/week1-web-frame/src/main/java/org/ipman/web/mvc/controller/PageController.java
@@ -0,0 +1,12 @@
+package org.ipman.web.mvc.controller;
+
+/**
+ * Created by ipipman on 2021/3/3.
+ *
+ * @version V1.0
+ * @Package org.ipman.web.mvc.controller
+ * @Description: (用一句话描述该文件做什么)
+ * @date 2021/3/3 11:13 上午
+ */
+public interface PageController extends Controller {
+}
